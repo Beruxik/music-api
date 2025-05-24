941c5922b5cfc45fd1857050896aa89a1158ff36
Beruxik <jakubik2911@gmail.com>
2025-05-24 11:23:15 +0200

    Add reading song by preference

--- app/routers/songs.py
+++ app/routers/songs.py
@@ -1,12 +1,12 @@
 import random
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session, select
 
 from app.config import setup_logger
 from app.database import get_session
-from app.models import Song
+from app.models import PreferenceGenreLink, Song, SongGenreLink
 
 SessionDep = Annotated[Session, Depends(get_session)]
 logger = setup_logger(__name__)
@@ -25,3 +25,26 @@ def read_songs(
 ) -> list[Song]:
     songs = session.exec(select(Song)).all()
     return random.sample(songs, limit)
+
+
+@router.get("/{preference_id}")
+def read_songs_by_preference(
+    preference_id: int,
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=10)] = 10,
+) -> list[Song]:
+    songs = session.exec(
+        select(Song)
+        .join(SongGenreLink, SongGenreLink.song_id == Song.id)
+        .join(
+            PreferenceGenreLink,
+            PreferenceGenreLink.genre_id == SongGenreLink.genre_id,
+        )
+        .where(PreferenceGenreLink.preference_id == preference_id)
+        .where(SongGenreLink.genre_id == PreferenceGenreLink.genre_id)
+        .where(SongGenreLink.song_id == Song.id)
+        .offset(offset)
+        .limit(limit),
+    ).all()
+    return songs  # noqa: RET504
