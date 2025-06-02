6a609785b32904ee392863f17a2b682ef4945046
Beruxik <jakubik2911@gmail.com>
2025-06-02 19:51:25 +0200

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 sys.path.append("..")
@@ -8,6 +9,18 @@ sys.path.append("..")
 from app.routers import preferences, songs
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(songs.router)
